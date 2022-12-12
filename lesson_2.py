1e96de35d1009b7811b1cb5726d57e57e3998563
Asror Kabulov <ansoriy559@gmail.com>
2022-12-13 00:32:53 +0600

    Create lesson_2.py

---
+++ lesson_2.py
@@ -0,0 +1,103 @@
+ # Операторы сравнения, функция input(), условия if и else, boolean, циклы
+# ==, >=, <=, <, >, !=
+# print(10 == 9)
+# print(10==10)
+# print("IT" == "IT")
+# print(10.0 == 10.0)
+# print(2 == 2.0)
+# print(True == True)
+# print(10 =="10")
+# print(int("10") == 10)
+# print(10>20)
+# print(10 <5)
+# print(10 != 9)
+# print(1000 != 1000)
+# print(50 >= 50)
+# print(100 >= 50)
+# print(30 <= 100)
+# print(30 <= 400 )
+
+# num1 = int(input("Введите число: ")) 
+# num2 = int(input("Введите число: "))
+# print(num1 + num2)
+
+# name = input("Введите имя:")
+# print("Здраствуйте",  name)
+
+# if,else,elif
+# if True:
+#     print("if true")
+# else:
+#     print("if false")    
+
+# num1 = int(input("Введите первое число:"))
+# num2 = int(input("Введите второе число:"))
+# if num1 > num2:
+#     print("первое число больше")
+# else:
+#     print(" Второе число больше")
+
+# num = int(input("Введите  число:"))
+# if num % 2 == 0:
+#     print(num, "четный")
+# else:
+#     print(num, "нечетный")    
+
+# # Логические операторы or и and 
+# num1 = 100
+# num2 = 100
+# num3 = 100
+# if num1 > num2 and num1 > num3:
+#     print("первое число больше")
+# elif num2 > num1 and num2 >num3:
+#     print("Второе число больше")
+# elif num3 > num1 and num3 > num2:
+#     print("Третое число больше")
+# else:
+#     print("Они равны")
+
+# login = input("Введите логин:")
+# password = input("Введите пароль:")
+# if login == "geektech" and password == "geek2021":
+#     print("Welcome")
+# else:
+#     print("Error")     
+
+# login = input("Введите логин:")
+# password = input("Введите пароль:")
+# if login == "geektech":
+#     if password == "geek2021":
+#         print("Добро пожаловать")
+#     else:
+#         print("пароль не совпадает")
+# else:
+#     print("Неверный логин")        
+
+
+# While True:
+# number1 = float(input("Введите первое число:"))
+# number2 = float(input("Введите второе число:"))
+# operator = input("+, -, *, /:")
+# if operator =="+":
+#     print(number1 + number2)
+# elif operator == "-":
+#     print(number1 - number2)    
+# if operator == "*":
+#     print(number1 * number2)
+# elif operator == "/":
+#     print(number1 / number2) 
+# else:
+#     print("Неправильный оператор")       
+
+# print(True + True - False)
+
+#Циклы for, while
+# for num in range(1001):
+#     print(num)
+ 
+
+# name = "ansor"
+# for i in name:
+#     print(i)
+#     if i == "a":
+#         print("В имени есть буква а")
